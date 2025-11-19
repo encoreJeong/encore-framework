ceb322475fab4b69fefc290ed7ff92184091140c
encoreJeong <zzaibean@gmail.com>
2025-11-19 16:42:56 +0900

    build(log): 로깅을 위한 의존성 설정 및 로깅 코드 추가

--- sample-app/src/main/java/com/example/app/controller/resthandler/RestGETController.java
+++ sample-app/src/main/java/com/example/app/controller/resthandler/RestGETController.java
@@ -3,10 +3,16 @@ package com.example.app.controller.resthandler;
 import com.encorejeong.encoreframework.web.handler.RestController;
 import com.encorejeong.encoreframework.web.request.vo.RequestParams;
 import java.io.IOException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class RestGETController implements RestController {
+
+    private static final Logger log = LoggerFactory.getLogger(RestGETController.class);
+
     @Override
     public Object handle(RequestParams params) throws IOException {
+        log.info("[RestGETController.handle]");
         return params;
     }
 }
